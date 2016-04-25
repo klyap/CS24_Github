033408dc2733981ee525129b6176525b73418260
klyap <kerleeyap@Kers-MacBook-Pro.local>
2016-04-25 12:34:17 -0700

    assembly code mostly done, last line unsure

--- CS24_HW4/exceptions/my_setjmp.s
+++ CS24_HW4/exceptions/my_setjmp.s
@@ -1,29 +1,39 @@
+/* Arguments and where they are stored 
+int my_setjmp(my_jmp_buf buf);
+void my_longjmp(my_jmp_buf buf, int ret);
+*/
+
 .globl setjmp	       /* Ignore everything beginning with a "." */
 .text
 	.align 4
-setjmp:
-	pushl %ebx         /* Push callee save registers. */
-	pushl %esi         /* Push callee save registers. */
-	pushl %edi         /* Push callee save registers. */
-	pushl %esp         /* Push stack frame. */
-	pushl %ebp         /* Push old base pointer. */
-	movl %esp,%ebp	   /* Current stack is new base. */
-	movl $0, %eax
+my_setjmp:
+	pushl %ebp         	/* Push old base pointer. */
+	movl %esp,%ebp	   	/* Current stack is new base. */
+	movl 8(%ebp), %ecx	/* Store jmp_buf. */
+	movl %esp, (%ecx)	/* Move to jmp_buf */
+	movl %ebx, 4(%ecx)         	/* Push callee save registers. */
+	movl %esi, 8(%ecx)         /* Push callee save registers. */
+	movl %edi, 12(%ecx)        /* Push callee save registers. */
+	movl %esp, 16(%ecx)        /* Push stack frame. */
+	
+	xor %eax, %eax		/* Set %eax to 0
 
-setjmp_return:
-	movl %ebp,%esp     /* Pop local stack. */
 	popl %ebp          /* Pop old base of frame. */
 	ret
 
-longjmp:
-	pushl %ebp         /* Push old base pointer. */
-	movl %esp,%ebp	   /* Current stack is new base. */
-	/* Go to env */
-	/* Restore callee save registers */
-	/* Set return value to second argument, 8(%ebp)*/
-
-longjmp_return:
-	movl %ebp,%esp     /* Pop local stack. */
+my_longjmp:
+	pushl %ebp         	/* Push old base pointer. */
+	movl %esp,%ebp	   	/* Current stack is new base. */
+	movl 12(%ebp), %eax	/* Store ret */
+	movl 8(%ebp), %ecx	/* Store jmp_buf */
+	movl 4(%ecx), %ebx         	/* Restore callee save registers. */
+	movl 8(%ecx), %esi          /* Restore callee save registers. */
+	movl 12(%ecx), %edi         /* Restore callee save registers. */
+	movl 16(%ecx), %esp        /* Restore stack frame. */
+	cmpl $0, %eax		/* If ret != 0 */
+	jnz done			/* go to return 0 */
+	movl $1, %eax		/* Else go to return 1 */
+done:
 	popl %ebp          /* Pop old base of frame. */
 	ret
 
