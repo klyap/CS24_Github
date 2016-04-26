6684a4306995513ae718f6af370c7eeec0c6d596
klyap <kerleeyap@Kers-MacBook-Pro.local>
2016-04-26 15:39:54 -0700

    wrote test for setjmp

--- CS24_HW4/exceptions/test_setjmp.c
+++ CS24_HW4/exceptions/test_setjmp.c
@@ -2,11 +2,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* 
- */
-void longjmp_return_1() {
+/* Used to jump back to main() if we get a bad input. */
+int corruption_check_left = 0;
+jmp_buf env;
+int corruption_check_right = 0;
+
+/* Computes sqrt(x – 3), as long as x >= 3. Otherwise,
+* performs a longjmp() back to an error handler.
+*/
+/*double compute(double x) {
+ if (x < 3)
+ longjmp(env, 1); //Jump to error handler!
+
+ return sqrt(x – 3);
+}*/
+
+
+/*void longjmp_return_1() {
     printf("longjmp(buf, 0) returns 1:    ");
-    if (longjmp(buf, 0) == 1) {
+    if (longjmp(env, 0) == 1) {
         printf("PASS\n");
     } else {
         printf("FAIL\n");
@@ -15,7 +29,7 @@ void longjmp_return_1() {
 
 void longjmp_return_n() {
     printf("longjmp(buf, n) returns n:    ");
-    if (longjmp(buf, 5) == 5) {
+    if (longjmp(env, 5) == 5) {
         printf("PASS\n");
     } else {
         printf("FAIL\n");
@@ -24,22 +38,61 @@ void longjmp_return_n() {
 
 void longjmp_between_multiple() {
     printf("longjmp across multiple functions:    ");
-    if (longjmp(buf, 5) == 5) {
+    if (longjmp(env, 5) == 5) {
         printf("PASS\n");
     } else {
         printf("FAIL\n");
     }
+}*/
+
+int f(int x){
+    if (setjmp(env) == 0){
+        return g(x);
+        printf("Regular setjmp\n");
+    } else if (setjmp(env) == 1){
+        printf("Returned 1 \n");
+        return -1;
+    } else if (setjmp(env) == 2) {
+        // Chose n = 2
+        printf("Returned n\n");
+        return -1;
+    } else {
+        printf("FAIL\n");
+        return -1;
+    }
 }
 
-// TODO: 4th test: check functions don't corrupt stack
+int g(int x){
+    return h(x);
+}
 
+int h(int x){
+    if (x == 1){
+        longjmp(env, 0);
+    } else if (x == 2){
+        longjmp(env, 2);
+    }
+    return -2;
+}
+
+void corruption_check(){
+    if (corruption_check_left == corruption_check_right){
+        printf("PASS\n");
+    } else {
+        printf("FAIL\n");
+    }
+
+}
 
 int main(int argc, char *argv[]) {
 	// TODO: Need functions with try/catch blocks and buf to test on
-	// Use divider and ptr_vector??
-    longjmp_return_1();
-    longjmp_return_n();
-    longjmp_between_multiple();
+	printf("Testing plain setjmp: ");
+    f(0);
+    printf("Longjmp returns 1: ");
+    f(1);
+    printf("Longjmp returns n / jumps multiple functions: ");
+    f(2);
+    printf("No corruption: ");
 
     return 0;
 }
