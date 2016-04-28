7d588a2eac949c12611f4d85b1a19d716eb92dea
klyap <kerleeyap@Kers-MacBook-Pro.local>
2016-04-27 23:58:49 -0700

    added a test case for returning n

--- CS24_HW4/exceptions/test_setjmp.c
+++ CS24_HW4/exceptions/test_setjmp.c
@@ -78,7 +78,7 @@ void jump_return_n(){
             longjmp(env, 2);
         }
         printf("    FAIL: Test jump return n\n");
-    } else if (setjmp(env) < 2) {
+    } else if (setjmp(env) == 1) {
         printf("    PASS: Test jump return n\n");
     } else {
         printf("    FAIL: Test jump return n\n");
