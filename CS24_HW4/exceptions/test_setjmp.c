5b5d48cf65485d1b300d25222c7c75cfe372d8ca
klyap <kerleeyap@Kers-MacBook-Pro.local>
2016-04-28 01:10:18 -0700

    added a test case to test_setjmp.c

--- CS24_HW4/exceptions/test_setjmp.c
+++ CS24_HW4/exceptions/test_setjmp.c
@@ -8,16 +8,10 @@ int corruption_check_left = 0;
 jmp_buf env;
 int corruption_check_right = 0;
 
-void corruption_check(){
-    printf("Testing jmp_buf is uncorrupted: \n");
-    if (corruption_check_left == corruption_check_right){
-        printf("    PASS: Testing jmp_buf is uncorrupted\n");
-    } else {
-        printf("    FAIL: Testing jmp_buf is uncorrupted\n");
-    }
-
-}
-
+/*
+Helper functions that long jumps back if x < 5 without returning.
+Otherwise, it returns x - 5.
+*/
 int h(int x){
     if (x < 5){
         longjmp(env, 1);
@@ -27,10 +21,16 @@ int h(int x){
     return x - 5;
 }
 
+/*
+Helper function that calls h(x)
+*/
 int g(int x){
     return h(15 - x);
 }
 
+/*
+  Tests case where no exceptions are thrown. It should call h(x) succesfully.
+*/
 int no_exceptions(){
     int x = 1;
     printf("Testing regular setjmp with no exceptions thrown: \n");
@@ -42,6 +42,11 @@ int no_exceptions(){
     }
 }
 
+/* 
+  Tests whether you can long jump across the various functions
+  (ie. g() and h()). h(x) should thrown an exception that
+  this function catches.
+*/
 int jump_across(){
     int x = 5;
     printf("Testing jumping across functions: \n");
@@ -54,6 +59,10 @@ int jump_across(){
     }
 }
 
+/* 
+  Tests whether you can long jump into back within
+  the same function.
+*/
 void jump_within(){
     int x = 1;
     printf("Test jump within function: \n");
@@ -69,15 +78,35 @@ void jump_within(){
 
 }
 
+/* Tests whether long jump returns n, which is defined
+  to be 2 in this test case.
+*/
 int jump_return_n(){
     printf("Test jump return n: \n");
     
     int curr_env = setjmp(env);
     if (curr_env == 0){
         longjmp(env, 2);
+    } else if (curr_env == 2){
+        printf("    PASS: Test jump within function\n");
+        return curr_env;
+    }
+    printf("    FAIL: Test jump within function\n");
+    return curr_env;
+}
+
+/* Tests whether the jump buffer was corrupted by checking
+ whether the two local variables on either side of it
+ are still what I had defined them to be initially.
+*/
+void corruption_check(){
+    printf("Testing jmp_buf is uncorrupted: \n");
+    if (corruption_check_left == corruption_check_right){
+        printf("    PASS: Testing jmp_buf is uncorrupted\n");
+    } else {
+        printf("    FAIL: Testing jmp_buf is uncorrupted\n");
     }
 
-    return curr_env;
 }
 
 int main(int argc, char *argv[]) {
@@ -92,11 +121,8 @@ int main(int argc, char *argv[]) {
     // Test 4
     corruption_check();
     // Test 5
-    if (jump_return_n() == 2){
-        printf("    PASS: Test jump within function\n");
-    } else {
-        printf("    FAIL: Test jump within function\n");
-    }
+    jump_return_n();
+
 
     return 0;
 }
